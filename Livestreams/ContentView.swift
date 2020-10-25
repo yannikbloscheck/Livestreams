9fcebe2688a43a0677a4f1fc112624ca0ac4d023
Yannik Bloscheck <yannikbloscheck@users.noreply.github.com>
2020-10-25 22:40:47 +0100

    Initial Commit

---
+++ Livestreams/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Livestreams
+//
+//  Created by Yannik Bloscheck on 2020-10-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Livestreams/LivestreamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivestreamsApp.swift
+//  Livestreams
+//
+//  Created by Yannik Bloscheck on 2020-10-25.
+//
+
+import SwiftUI
+
+@main
+struct LivestreamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
